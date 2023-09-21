7806922bc68cfc328015b278ef4a97039ebc84a7
plgafhd <plgafhd@snu.ac.kr>
2023-09-21 21:07:29 +0900

    c

---
+++ app/src/main/java/com/example/assignment1/CalculatorActivity.kt
@@ -0,0 +1,76 @@
+package com.example.assignment1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.GridLayout
+import android.widget.LinearLayout
+import android.widget.TextView
+
+class CalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+
+        val intent: Intent =getIntent()
+        val useridtext: TextView = findViewById<TextView>(R.id.userInfo_ID)
+        val userpwtext: TextView = findViewById<TextView>(R.id.userInfo_PW)
+        useridtext.setText(intent.getStringExtra("ID"))
+        userpwtext.setText(intent.getStringExtra("PW"))
+
+        val term1text:EditText=findViewById<EditText>(R.id.term1)
+        val term2text:EditText=findViewById<EditText>(R.id.term2)
+        val resulttext:TextView=findViewById<TextView>(R.id.result)
+        val operatortext:TextView=findViewById<TextView>(R.id.operator)
+        var term1:String
+        var term2:String
+
+        val plusbutton:Button=findViewById<Button>(R.id.operator_plus)
+        plusbutton.setOnClickListener{
+            term1=term1text.getText().toString()
+            term2=term2text.getText().toString()
+            if (term1!="" && term2!=""){
+                resulttext.setText((term1.toInt() + term2.toInt()).toString())
+                operatortext.setText("+")
+            }
+        }
+
+        val minusbutton:Button=findViewById<Button>(R.id.operator_minus)
+        minusbutton.setOnClickListener{
+            term1=term1text.getText().toString()
+            term2=term2text.getText().toString()
+            if (term1!="" && term2!=""){
+                resulttext.setText((term1.toInt() - term2.toInt()).toString())
+                operatortext.setText("-")
+            }
+        }
+
+        val multbutton:Button=findViewById<Button>(R.id.operator_mult)
+        multbutton.setOnClickListener{
+            term1=term1text.getText().toString()
+            term2=term2text.getText().toString()
+            if (term1!="" && term2!=""){
+                resulttext.setText((term1.toInt() * term2.toInt()).toString())
+                operatortext.setText("×")
+            }
+        }
+
+        val divbutton:Button=findViewById<Button>(R.id.operator_div)
+        divbutton.setOnClickListener{
+            term1=term1text.getText().toString()
+            term2=term2text.getText().toString()
+            if (term1!="" && term2!=""){
+                operatortext.setText("÷")
+                if (term2=="0"){
+                    resulttext.setText("Error")
+                }
+                else {
+                    resulttext.setText((term1.toInt() / term2.toInt()).toString())
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.assignment1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val loginbutton:Button=findViewById<Button>(R.id.LoginButton)
+        val idtext:EditText=findViewById<EditText>(R.id.Login_ID)
+        val pwtext:EditText=findViewById<EditText>(R.id.Login_PW)
+
+        loginbutton.setOnClickListener{
+            val user_id:String=idtext.getText().toString()
+            val user_pw:String=pwtext.getText().toString()
+            if (user_pw.length<5){
+                Toast.makeText(this@MainActivity,"유효하지 않은 비밀번호입니다",Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val intent=Intent(this,CalculatorActivity::class.java)
+                intent.putExtra("ID", user_id)
+                intent.putExtra("PW", user_pw)
+                startActivity(intent)
+            }
+        }
+    }
+}
